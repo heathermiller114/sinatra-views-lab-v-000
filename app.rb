20d8a24e6a4f8757ce526becc9c4756eab1f41ba
Heather Miller <heather.miller114@gmail.com>
2019-01-07 00:14:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,8 +6,8 @@ class App < Sinatra::Base
   get '/hello' do
     erb :hello
   end
-  get '/goodybe' do
-    erb :goodybe
+  get '/goodbye' do
+    erb :goodbye
   end
   get '/date' do
     erb :date
